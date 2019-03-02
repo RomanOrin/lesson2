7ca6597bd310ed59426081fe0239c4af37b882cc
Roman Orin <roman.orin@mail.ru>
2019-03-02 11:57:28 +0300

    Updated one file

--- while-looping1.py
+++ while-looping1.py
@@ -1,13 +1,10 @@
-real_answer = 'fokfo'
-
-def ask_user(answer):
+def ask_user():
     while True:
         user_say = input('How are you? ')
         if user_say == 'Good':
             break
-
-print(ask_user(real_answer))
-
+    return user_say
+ask_user()
 
 
 
